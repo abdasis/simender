7c75cd3e03fa11eeabca89ca5131a463d4314b0f
Abd. Asis <34927165+abdasis@users.noreply.github.com>
2021-09-19 14:47:31 +0700

    update logo dan bg

--- app/Http/Livewire/PendaftaranSim.php
+++ app/Http/Livewire/PendaftaranSim.php
@@ -52,7 +52,7 @@ class PendaftaranSim extends Component
             $isi_pesan = NotifSim::first();
 
             $diganti = [
-                ':jatuh_tempo' => $this->pajak_tahunan,
+                ':jatuh_tempo' => $this->masa_berlaku,
                 ':jenis_sim' => $this->jenis_sim,
                 ':alamat' => $this->alamat,
                 'jatuh_tempo' => $jatuh_tempo,
